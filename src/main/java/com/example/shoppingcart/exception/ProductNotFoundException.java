86dc0a01c958998abcdfdf2b8c0e87d17575ead8
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2025-07-31 12:06:33 -0400

    Delete src directory

--- src/main/java/com/example/shoppingcart/exception/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-// Custom exception for product not found
-package com.example.shoppingcart.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/shoppingcart/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-// Product repository interface
-package com.example.shoppingcart.repository;
-
-import com.example.shoppingcart.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
